a0d8863fcc7e0d18e18a7d45b7896a53fba5cde2
SaranKaewchuay <101935507+SaranKaewchuay@users.noreply.github.com>
2023-07-09 15:15:39 +0700

    Update movieDetail.js

--- src/screen/movieDetail.js
+++ src/screen/movieDetail.js
@@ -20,38 +20,33 @@ const MovieDetail = () => {
       <div className="container">
         <div className="margin">
           <div className="row ">
-            <div className="column-contents flex-container">
+            
+            <div className="column-contents flex-container col-3">
               <img className="image-card" src={data.image_url} alt="Movie Poster" />
             </div>
-            <div className="flex-container">
+            <div className="flex-container col shadow p-3 mb-5 bg-white rounded">
               <div className="margin-left">
                 <div className="margin-content ">
-                  <p className="title">{data.title}</p>
+                  <p className="title" style={{fontSize:"50px"}}>{data.title}</p>
                 </div>
                 <div className="margin-content ">
-                  <span className="font-size-content">{data.rating} </span>
-
-                  <span style={{ marginLeft: "20px" }}>
-                    <img
-                      className="icon-image"
-                      src="https://img.icons8.com/android/48/calendar.png"
-                      alt="calendar"
-                    />
-                  </span>
-                  <span className="font-size-content">{data.show_time}:00 </span>
-
-                  <span style={{ marginLeft: "20px" }}>
-                    <img
-                      className="icon-image"
-                      src="https://img.icons8.com/material-outlined/24/clock--v1.png"
-                      alt="clock"
-                    />
-                  </span>
-
-                  <span className="font-size-content">{data.duration} </span>
+                  <span className="font-size-content" style={{fontSize:"20px",fontWeight:"bolder"}}>Rating :  </span>
+                  <span className="font-size-content" style={{fontSize:"20px"}}>{data.rating} </span>
+                  <br/>
+                  <span className="font-size-content" style={{fontSize:"20px",fontWeight:"bolder"}}>ShowTime :  </span>
+                  <span className="font-size-content" style={{fontSize:"20px"}}>{data.show_time}:00 </span>
+                  <br/>
+                  <span className="font-size-content" style={{fontSize:"20px",fontWeight:"bolder"}}>Cast :  </span>
+                  <span className="font-size-content" style={{fontSize:"20px"}}>{data.cast} </span>
+                  <br/>
+                  <span className="font-size-content" style={{fontSize:"20px",fontWeight:"bolder"}}>Director :  </span>
+                  <span className="font-size-content" style={{fontSize:"20px"}}>{data.director} </span>
+                  <br/>
+                  <span className="font-size-content" style={{fontSize:"20px",fontWeight:"bolder"}}>Duration :  </span>
+                  <span className="font-size-content" style={{fontSize:"20px"}}>{data.duration} </span>
                 </div>
                 <div className="margin-content ">
-                  <p className="font-size-content">{data.description}</p>
+                  <p className="font-size-content" style={{fontSize:"20px"}}>{data.description}</p>
                 </div>
               </div>
             </div>
