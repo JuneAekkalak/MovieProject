0629137c163388478026a28a13ca304b2c77efaf
SaranKaewchuay <saran.ka@mail.wu.ac.th>
2023-07-09 13:51:31 +0700

    add f

--- src/screen/favorite.js
+++ src/screen/favorite.js
@@ -22,51 +22,62 @@ const Favorites = () => {
   };
 
   return (
-    <div className="mt-5 container">
-      <div
-        className="shadow p-3 mb-5 bg-white rounded"
-        style={{ width: "500px" }}
-      >
-        <p style={{ fontSize: "25px" }}>Favorites List</p>
-      </div>
-      <div className="flex flex-wrap">
-        {favoriteItems.length > 0 ? (
-          favoriteItems.map((item) => (
-            <div key={item.id} className="w-1/2 p-4">
-              <div className="flex items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                <img
-                  className="object-cover w-1/2 rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                  src={item.image_url}
-                  alt=""
-                />
-                <div className="flex flex-col justify-between p-4 leading-normal w-1/2">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    {item.title}
-                  </h5>
-                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+    <div className="mt-5 container mb-5" >
+    <div className="shadow p-3 mb-5 bg-white rounded" style={{ width: "500px" }}>
+      <p style={{ fontSize: "25px" }}>Favorites List</p>
+    </div>
+    <div className="flex flex-wrap lg:flex-nowrap justify-center">
+      {favoriteItems.length > 0 ? (
+        <div className="container-fluid">
+          <div className="row">
+            {favoriteItems.map((item) => (
+              <div className="col-6 mt-3" key={item.id}>
+                <div className="card">
+                  <div className="card-horizontal">
+                    <div className="img-square-wrapper">
+                      <img
+                        className=""
+                        src={item.image_url}
+                        alt="Card image cap"
+                        style={{ width: "150px" }}
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      {item.title}
+                    </h5>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                     {item.director}
                   </p>
                   <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                     {item.rating}
                   </p>
-
-                  <button
-                    className="btn btn-danger absolute top-4 right-4"
-                    onClick={() => removeFromFavorites(item.id)}
+                    </div>
+                  </div>
+                  <div
+                    className="card-footer"
+                    style={{ position: "absolute", top: "4px", right: "4px" }}
                   >
-                    Remove from Favorites
-                  </button>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => removeFromFavorites(item.id)}
+                    >
+                      Remove from Favorites
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))
-        ) : (
-          <div className="mt-5">
-            <p>No favorite items found.</p>
+            ))}
           </div>
-        )}
-      </div>
+        </div>
+      ) : (
+        <div className="mt-5">
+          <p>No favorite items found.</p>
+        </div>
+      )}
     </div>
+  </div>
+  
   );
 };
 
